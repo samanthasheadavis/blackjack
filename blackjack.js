b635996a6374588e24f20760143d990dc48c2724
samanthasheadavis <samantha.shea.davis@gmail.com>
2016-09-28 18:43:53 -0400

    fixed bugs

--- blackjack.js
+++ blackjack.js
@@ -2,6 +2,7 @@ function runGame() {
     //function to pick a random number and assign it to a card in the deck.
     var display = document.getElementById('cards');
     var cards = ['A', '2', '3', '4', '5', '6', '7', '8', '9', 'J', 'Q', 'K'];
+    var cardValue = 0;
 
     function hit() {
         checkResult(false, true);
@@ -17,40 +18,43 @@ function runGame() {
      * @return {void}
      */
     function checkResult(standing, hitting) {
-        //card = display.innerHTML.split(' ');
+        hand = display.innerHTML.split(' ');
         //  var cards = card or cards currently listed on the display.
 
-        var cardValue = 0;
-        var i = 0;
 
-        cards.forEach(function(card, i) {
+        hand.forEach(function(card, i) {
 
-            if (card[i] === 'J' || card[i] === 'Q' || card[i] === 'K') {
-                cardValue = 10;
-
+            if (card[i] === 'J') {
+                cardValue += 10;
+            } else if (card[i] === 'Q') {
+                cardValue += 10;
+            } else if (card[i] === 'K') {
+                cardValue += 10;
             } else if (card[i] === 'A') {
-                cardValue = 15;
-
-            } else {
-                cardValue = cardValue + card;
+                cardValue += 11;
+            } else if (Number(card)) {
+                cardValue = cardValue + Number(card);
             }
-//if (Number(card[i]))
+
         });
         console.log('cardvalue is: ' + cardValue);
         console.log('card is: ' + card);
 
-        if (cardValue < 15 && standing) {
-            alert('Dealer wins.');
+          if (cardValue < 15 && standing) {
+              alert('Dealer wins.');
+          }
+        else if (cardValue < 18 && standing) {
+              alert('Push!');
+          }
+        else if (cardValue > 18 && hitting) {
+              alert('You win!');
+          }
+        else if (cardValue === 21) {
+              alert('You win!');
         }
-      else if (cardValue < 18 && standing) {
-            alert('Push!');
-        }
-      else if (cardValue > 18 && hitting || cardValue === 21) {
-            alert('You win!');
-        }
-      else if (cardValue > 21) {
-            alert('You Bust.');
-        }
+        else if (cardValue > 21) {
+              alert('You Bust.');
+          }
 
         display.innerHTML = ' ';
         card = Math.round(Math.random() * cards.length);
@@ -59,7 +63,7 @@ function runGame() {
     }
     //   If stand button is clicked, trigger stand function
     document.getElementById('stand').addEventListener('click', function() {
-        checkResult(false, true);
+        checkResult(true, false);
     });
 
     //If hit button is clicked, trigger hit function
@@ -67,11 +71,11 @@ function runGame() {
         checkResult(false, true);
 
     });
-    // }
+
     // Start the game with a hand of two cards
     card = Math.round(Math.random() * cards.length);
     display.innerHTML = cards[card];
     card = Math.round(Math.random() * cards.length);
-    display.innerHTML = display.innerHTML + '  ' + cards[card];
+    display.innerHTML = display.innerHTML + ' ' + cards[card];
 }
-runGame();
+ runGame();
